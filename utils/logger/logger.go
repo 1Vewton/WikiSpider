53cc212ef87173f767dbe07f0265fb044758aa14
NewtonZhan <zhanyunze0601@gmail.com>
2026-06-03 08:52:33 +0800

    feat(logger): Add logger

---
+++ utils/logger/logger.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+)
+
+var handler *slog.TextHandler
+var logger *slog.Logger
+
+// initialize logger
+func init() {
+	opts := &slog.HandlerOptions{AddSource: true}
+	_, err := os.Stat("app.log")
+	switch {
+	case err == nil:
+		// Directly open the file
+		writer, err := os.OpenFile("app.log", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+		opts.Level = slog.LevelInfo
+		handler = slog.NewTextHandler(io.MultiWriter(writer, os.Stdout), opts)
+	case os.IsNotExist(err):
+		_, err := os.Create("app.log")
+		if err != nil {
+			panic(err)
+		}
+		opts.Level = slog.LevelDebug
+		writer, err := os.OpenFile("app.log", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+		handler = slog.NewTextHandler(io.MultiWriter(writer, os.Stdout), opts)
+	default:
+		panic(err)
+	}
+	logger = slog.New(handler)
+}
+
+// info
+func Info(msg string, service string) {
+	logger.Info(fmt.Sprintf("%s: %s", service, msg))
+}
---
+++ wikispider.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/1Vewton/WikiSpider/utils/logger"
+)
+
+func main() {
+	logger.Info("Program started", "WikiSpider")
+}
